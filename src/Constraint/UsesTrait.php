4daaaa6a5ef53f3d0f35ab6c6ce2bc5e3bb1f471
ptomulik <pawel@tomulik.pl>
2026-05-12 08:12:39 +0000

    Merge branch '2.0' into 3.0

--- src/Constraint/UsesTrait.php
+++ src/Constraint/UsesTrait.php
@@ -20,36 +20,23 @@ final class UsesTrait extends AbstractConstraint
 {
     use ConstraintImplementationTrait;
 
-    /**
-     * @var string
-     */
-    private static $verb = 'uses trait';
+    private static string $verb = 'uses trait';
+    private static string $negatedVerb = 'does not use trait';
 
     /**
-     * @var string
-     */
-    private static $negatedVerb = 'does not use trait';
-
-    /**
-     * @var array
-     *
      * @psalm-var array{0:callable, 1:string}
      */
-    private static $validation = ['trait_exists', 'a trait-string'];
+    private static array $validation = ['trait_exists', 'a trait-string'];
 
     /**
-     * @var callable
-     *
      * @psalm-var callable
      */
-    private static $inheritance = 'class_uses';
+    private static mixed $inheritance = 'class_uses';
 
     /**
-     * @var array
-     *
      * @psalm-var array{0:callable, 1:callable}
      */
-    private static $supports = ['class_exists', 'trait_exists'];
+    private static array $supports = ['class_exists', 'trait_exists'];
 }
 
 // vim: syntax=php sw=4 ts=4 et:
